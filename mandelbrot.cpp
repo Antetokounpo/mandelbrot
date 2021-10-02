65437deb7dde9a453b34435b6967b59a39c59a66
Antetokounpo <antor.232@outlook.com>
2021-10-02 00:33:41 -0400

    Small fix

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -32,7 +32,7 @@ struct rgb hsv2rgb(struct hsv color)
 
     float chroma = color.v * color.s;
     float hue = color.h / 60.0f;
-    float x = chroma * (1 - std::abs((int)hue % 2 - 1));
+    float x = chroma * (1 - std::abs(std::fmod(hue, 2) - 1));
 
     switch ((int)std::floor(hue))
     {
@@ -59,10 +59,6 @@ struct rgb hsv2rgb(struct hsv color)
     }
 
     float m = color.v - chroma;
-
-    std::cout << out_color.r + m << std::endl;
-    std::cout << out_color.g + m << std::endl;
-    std::cout << out_color.b + m << std::endl;
 
     return {out_color.r + m, out_color.g + m, out_color.b + m};
 }
@@ -107,11 +103,11 @@ int main()
 {
     std::complex<long double> center(RCENTER, ICENTER);
     std::complex<long double> c;
-    double color;
+    float color;
     static uint pixels[YRES][XRES][3]; // static pour pas overflow
 
     float saturation = 0.9f;
-    float hue = 350.0f;
+    float hue = 320.0f;
 
     for(int y = 0; y<YRES; ++y)
     {
@@ -120,15 +116,11 @@ int main()
             c = pixel_to_cplx(x, y, XRES, YRES, center, ZOOM);
             color = (float)iterate(c, ITERATIONS) / (float)ITERATIONS;
 
+            struct rgb pixel_color = hsv2rgb({hue, saturation, color});
 
-            float value = color;
-            std::cout << hue << std::endl;
-
-            struct rgb pixel_color = hsv2rgb({hue, saturation, value});
-
-            pixels[y][x][0] = pixel_color.r * 256;
-            pixels[y][x][1] = pixel_color.g * 256;
-            pixels[y][x][2] = pixel_color.b * 256;
+            pixels[y][x][0] = pixel_color.r * 255;
+            pixels[y][x][1] = pixel_color.g * 255;
+            pixels[y][x][2] = pixel_color.b * 255;
         }
     }
 
